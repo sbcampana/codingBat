9c835cfa877828d91669b6453b09d8eede223cf5
Justin Cooper <cooper.justin13@svvsd.org>
2019-03-10 19:18:53 -0600

    commiting

---
+++ src/Step.java
@@ -0,0 +1,29 @@
+/**
+ * Created by Mark on 2/26/2019.
+ */
+
+
+public class Step {
+    public static void main(String [] args) {
+
+        // bigHeights([5, 3, 6, 7, 2], 2, 4) → 1
+        int[] nums = new int[6];
+        nums[0] = 3;
+        nums[1] = 5;
+        nums[2] = 6;
+        nums[3] = 7;
+        nums[4] = 2;
+
+        System.out.println("BIG STEPS: " + bigHeights(nums, 2, 4));
+    }
+
+    public static int bigHeights(int[] heights, int start, int end) {
+        int count = 0;
+        for (int i = start; i < end; i++){
+            if ((Math.abs(heights[i + 1] - heights[i])) >= 5){
+               count++;
+            }
+        }
+        return count;
+    }
+}
