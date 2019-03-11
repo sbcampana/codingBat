6d024133af684b779d3ec8dde8f74e4c65083d1b
Scott Campana <sbcampana@gmail.com>
2019-03-10 19:55:37 -0600

    added last print statement

--- src/Step.java
+++ src/Step.java
@@ -18,6 +18,8 @@ public class Step {
         System.out.println("2x Big Steps: " + bigHeights(nums, 2, 4) + 3);
         System.out.println("Also big 2x Big Steps: " + bigHeights(nums, 2, 4) + 3);
 
+        System.out.println("We found the number of big steps");
+
     }
 
     public static int bigHeights(int[] heights, int start, int end) {
